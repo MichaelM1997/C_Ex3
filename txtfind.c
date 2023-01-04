90916df2d5b2449dd1e244f2491188decbd9ebf0
MichaelM1997 <117936442+MichaelM1997@users.noreply.github.com>
2023-01-04 22:20:54 +0200

    Update txtfind.c

--- txtfind.c
+++ txtfind.c
@@ -1,87 +1,64 @@
 #include <stdio.h>
 #include <string.h>
 
+#define LINE 256 // maximum length of the input line
 #define WORD 30
-#define LINE 256
 
-int getWord(char s[]);
-int get_Line(char s[]);
-int substring( char *str1, char *str2);
-int similar(char *s, char *t, int n);
-void print_lines(char *str);
-void print_similar_words(char *str);
+// getline: read a line into s, return length
+int get_line(char s[]){
+    int i = 0,ch = 0;
 
-int main(){
-    char word[WORD];
-    char choice;
-    char l;
-    scanf("%s %c%c",word, &choice, &l);
-
-    switch (choice) {
-        case 'a':
-            print_lines(word);
-            break;
-        case 'b':
-            print_similar_words(word);
-            break;
-        default:
-            printf("Choose a or b");
-            break;
-    }
-    return 0;
-}
-
-int get_Line(char s[]) {
-    int numOfChars = 0;
-    int ch = 0;
-    while ((ch != '\n') && (ch != '\r')  && (numOfChars < LINE - 1)) {
-        if ((ch = getchar()) != EOF){
-            if (ch != '\r'){
-                s[numOfChars] = (char) ch;
-            }
-            else{
-                s[numOfChars] = '\n';
+    while ( i < LINE - 1 && ch != '\n' && ch != '\r'){
+        if((ch = getchar()) != EOF) {
+            if (ch != '\r') {
+                s[i] = ch;
+            } else {
+                s[i] = '\n';
             }
         }
-        else{
+        else {
             return 0;
         }
-    numOfChars++;
+        i++;
     }
-    if (numOfChars == 0){
+    
+    if (i==0){
         return 1;
     }
-    s[numOfChars] = '\0';
-    return numOfChars;
+    
+    s[i] = '\0';
+    return i;
 }
 
-int getWord(char s[]) {
-    int numOfChars = 0;
-   while(numOfChars < WORD){
-        if(scanf("%c",&w[numOfChars]) == EOF){
-            w[numOfChars] = '\0';
-            break;
-        }
-        else if(w[numOfChars] == '\n' || w[numOfChars] == '\t' || w[numOfChars] == ' '){
-            w[numOfChars] = '\0';
-            break;
-        }
+// getword: read a word into w, return length
+int getword(char w[])
+{
+    int ch, i = 0;
+
+    while ( i < WORD - 1 && (ch = getchar()) != EOF && ch != ' ' && ch != '\t' && ch != '\n'){
+        w[i] = ch;
+        i++;
     }
-    if (numOfChars == WORD){
-        w[numOfChars - 1] = '\0';
-    }
-    return numOfChars;
+        
+    // Handle empty line
+    if (ch == '\n' && i == 0)
+        w[i++] = ch;
+    w[i] = '\0';
+    return i;
 }
 
-int substring( char *str1, char *str2) {
+// substring: return 1 if str1 is a substring of str2, 0 otherwise
+int substring(char *str1, char *str2){
+
     if(strstr(str1, str2) != NULL) {
         return 1;
     }
     return 0;
 }
 
+// similar: return 1 if it is possible to get t from s by removing at most n characters, 0 otherwise
 int similar(char *s, char *t, int n){
-    //identical strings and zero letters to omit.
+    
     if (strcmp(s, t) == 0 && n == 0) {
         return 1;
     }
@@ -104,22 +81,46 @@ int similar(char *s, char *t, int n){
         i++;
     }
     return 1;
-}
 
-void print_lines(char *str){
-    char temp_line[LINE];
-    while(get_Line(temp_line) > 0){
-        if(substring(str, temp_line)){
-            printf("%s", temp_line);
+}
+// print_lines: print all the lines that contain the string str
+void print_lines(char *str)
+{
+    char temp_line[LINE]; // character array to hold the input temp_line
+
+    while (get_line(temp_line) > 0)
+    {
+        if (substring(str, temp_line)){
+            printf("%s", temp_line); // print the temp_line if it contains str
         }
     }
 }
 
-void print_similar_words(char *str){
-    char temp_word[WORD];
-    while (getWord(temp_word) > 0){
-        if (similar(temp_word, str, 1)){
-            printf("%s\n", temp_word);
+void print_similar_words(char *str) {
+    char temp_word[WORD]; // to store the words read from the input
+    while (getword(temp_word) > 0) { // read a temp_word from the input
+        if (similar(temp_word, str, 1)) { // check if temp is similar to str
+            printf("%s\n", temp_word); // print temp_word if it is similar to temp
         }
     }
 }
+
+int main() {
+    char word[WORD];
+    //choice for the users choise and l for the blank line '\n'
+    char choice,l;
+    //Gets the user choose
+    scanf("%s %choice%choice", word, &choice, &l);
+
+
+    if (choice == 'a') {
+        print_lines(word);
+    } else if (choice == 'b') {
+        print_similar_words(word);
+    } else {
+        printf("choose a or b");
+    }
+
+    return 0;
+}
+
